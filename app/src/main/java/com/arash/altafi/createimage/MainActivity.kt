b466a87376c7a4a84e545ce216335fd3123dac6f
Arash Altafi <arashaltafi1377@gmail.com>
2023-03-31 02:46:59 +0430

    Commit Save Image

--- app/src/main/java/com/arash/altafi/createimage/MainActivity.kt
+++ app/src/main/java/com/arash/altafi/createimage/MainActivity.kt
@@ -2,26 +2,11 @@ package com.arash.altafi.createimage
 
 import android.content.*
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
-import android.media.MediaScannerConnection
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.webkit.MimeTypeMap
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.arash.altafi.createimage.databinding.ActivityMainBinding
-import com.arash.altafi.createimage.utils.createUriFromBitmap
-import com.arash.altafi.createimage.utils.shareTextWithImage
-import com.arash.altafi.createimage.utils.shareTextWithVideo
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStream
-import java.util.*
-
+import com.arash.altafi.createimage.utils.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,162 +37,18 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
 
-            share(bitmap)
-            download()
-        }
-    }
+            btnShareImage.setOnClickListener {
+                shareTextWithImage(bitmap, "body", "title", "subject")
+            }
 
-    private fun share(bitmap: Bitmap) = binding.apply {
-        btnShareImage.setOnClickListener {
-            shareTextWithImage(packageName, bitmap, "body", "title", "subject")
-        }
+            btnSaveImageToDownload.setOnClickListener {
+                saveImageFromBitmapToDownload(bitmap, "imageName.jpg")
+            }
 
-        btnShareVideo.setOnClickListener {
-            shareTextWithVideo(
-                packageName,
-                "https://www.leader.ir/media/film/19/12/31912_364.mp4",
-                "body",
-                "title",
-                "subject"
-            )
-//                downloadAndShareVideo("https://www.leader.ir/media/film/19/12/31912_364.mp4")
-        }
-
-        btnShareMusic.setOnClickListener {
-
-        }
-
-        btnShareFile.setOnClickListener {
-
-        }
-    }
-
-    private fun download() = binding.apply {
-        btnDownloadAndInstallApk.setOnClickListener {
-
-        }
-
-        btnSaveImage.setOnClickListener {
-
-        }
-
-        btnSaveVideo.setOnClickListener {
-
-        }
-
-        btnSaveMusic.setOnClickListener {
-
-        }
-
-        btnSaveFile.setOnClickListener {
-
-        }
-    }
-
-    // Save a bitmap or a Uri to the "myFolder" directory in the root of the external storage
-    private fun saveToInternalStorage(context: Context, input: Any) {
-        val folderName = "myFolder"
-
-        // Create the directory if it doesn't already exist
-        val directory = File(Environment.getExternalStorageDirectory(), folderName)
-        if (!directory.exists()) {
-            directory.mkdirs()
-        }
-
-        // Get the output file name and extension
-        val extension = if (input is Bitmap) "png" else "jpg"
-        val fileName = UUID.randomUUID().toString() + ".$extension"
-
-        // Get the output file path
-        val filePath = File(directory, fileName).absolutePath
-
-        // Open an output stream to the file
-        val outputStream: OutputStream = FileOutputStream(filePath)
-
-        // Write the image data to the file
-        when (input) {
-            is Bitmap -> input.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-            is Uri -> {
-                val inputStream = context.contentResolver.openInputStream(input)
-                BitmapFactory.decodeStream(inputStream)
-                    ?.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-                inputStream?.close()
+            btnSaveImageToCache.setOnClickListener {
+                saveImageFromBitmapToCache(bitmap, "imageName.jpg")
             }
         }
-
-        // Close the output stream
-        outputStream.flush()
-        outputStream.close()
-
-        // Scan the file so that it shows up in the gallery app
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            MediaScannerConnection.scanFile(context, arrayOf(filePath), null, null)
-        } else {
-            context.sendBroadcast(
-                Intent(
-                    Intent.ACTION_MEDIA_MOUNTED,
-                    Uri.parse("file://${Environment.getExternalStorageDirectory()}")
-                )
-            )
-        }
-
-        // Show a toast message indicating that the file has been saved
-        Toast.makeText(context, "File saved to $folderName/$fileName", Toast.LENGTH_SHORT).show()
-
-    }
-
-    private fun saveToInternalStorageAndroid10Above(context: Context, bitmap: Bitmap) {
-        val resolver = context.contentResolver
-        val imageName = "my_image.png"
-        val imageDescription = "My image description"
-        val imageCollection =
-            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-        val newImageDetails = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, imageName)
-            put(MediaStore.Images.Media.DESCRIPTION, imageDescription)
-            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
-            put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
-            put(MediaStore.Images.Media.DATE_MODIFIED, System.currentTimeMillis() / 1000)
-        }
-        val newImageUri = resolver.insert(imageCollection, newImageDetails)
-        resolver.openOutputStream(newImageUri!!).use { outputStream ->
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-        }
-    }
-
-    private fun saveImageToPictures(bitmap: Bitmap, uri: Uri?) {
-        val folderName = "Pictures"
-        val directory = when {
-            uri != null -> {
-                File(uri.path!!)
-            }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-            }
-            else -> {
-                val picturesDirectory =
-                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-                File(picturesDirectory.absolutePath + File.separator + folderName)
-            }
-        }
-
-        if (!directory.exists()) {
-            directory.mkdirs()
-        }
-
-        val extension = if (uri != null) {
-            MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(uri))
-        } else {
-            "jpg"
-        }
-        val fileName = UUID.randomUUID().toString() + ".$extension"
-        val filePath = File(directory, "fileName.png").absolutePath
-        val outputStream = FileOutputStream(filePath)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-        outputStream.close()
-
-        // Show a toast message indicating the save was successful
-        Toast.makeText(this, "Image saved to $filePath", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/arash/altafi/createimage/utils/Ext.kt
+++ app/src/main/java/com/arash/altafi/createimage/utils/Ext.kt
@@ -1,19 +1,17 @@
 package com.arash.altafi.createimage.utils
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.app.DownloadManager
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
+import android.os.Environment
 import android.os.StrictMode
 import android.widget.Toast
 import androidx.core.content.FileProvider
 import java.io.File
 import java.io.FileOutputStream
-import java.net.URL
 
 fun Context.createUriFromBitmap(bitmap: Bitmap): Uri? {
     val file = File(externalCacheDir, System.currentTimeMillis().toString() + ".jpg")
@@ -28,7 +26,6 @@ fun Context.createUriFromBitmap(bitmap: Bitmap): Uri? {
 }
 
 fun Context.shareTextWithImage(
-    applicationId: String,
     bitmap: Bitmap,
     body: String,
     title: String,
@@ -42,7 +39,7 @@ fun Context.shareTextWithImage(
         Uri.fromFile(file)
     } else {
         FileProvider.getUriForFile(
-            this, "$applicationId.fileprovider", file
+            this, "$packageName.fileprovider", file
         )
     }
 
@@ -62,125 +59,35 @@ fun Context.shareTextWithImage(
     startActivity(shareIntent)
 }
 
-fun Activity.shareTextWithVideo(
-    applicationId: String,
-    videoUrl: String,
-    body: String,
-    title: String,
-    subject: String
-) {
-    val file = File(externalCacheDir, System.currentTimeMillis().toString() + ".mp4")
+fun Context.saveImageFromBitmapToCache(bitmap: Bitmap, imageName: String) {
+    val cacheDir = cacheDir
+    val cacheFile = File(cacheDir, imageName)
+    val fileOutputStream = FileOutputStream(cacheFile)
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+    fileOutputStream.flush()
+    fileOutputStream.close()
 
-    Thread {
-        URL(videoUrl).openStream().use { inputStream ->
-            file.outputStream().use { outputStream ->
-                inputStream.copyTo(outputStream)
-            }
-        }
-
-        runOnUiThread {
-            val videoUri = if (Build.VERSION.SDK_INT < 24) {
-                Uri.fromFile(file)
-            } else {
-                FileProvider.getUriForFile(
-                    this, "$applicationId.fileprovider", file
-                )
-            }
-
-            val sendIntent = Intent().apply {
-                action = Intent.ACTION_SEND
-                type = "video/mp4"
-                putExtra(Intent.EXTRA_TEXT, title + "\n\n" + body)
-                putExtra(Intent.EXTRA_TITLE, title)
-                putExtra(Intent.EXTRA_SUBJECT, subject)
-                putExtra(Intent.EXTRA_STREAM, videoUri)
-            }
-            val chooserIntent = Intent.createChooser(sendIntent, "Share Video")
-            chooserIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-            startActivity(chooserIntent)
-        }
-    }.start()
-}
-
-@SuppressLint("Range")
-fun Activity.downloadAndShareVideo(videoUrl: String) {
-    // Create a File object from the external cache directory and the video file name
-    val videoFile = File(externalCacheDir, System.currentTimeMillis().toString() + ".mp4")
-
-    // Create a DownloadManager.Request object to start the download
-    val downloadRequest = DownloadManager.Request(Uri.parse(videoUrl))
-        .setAllowedOverMetered(true)
-        .setAllowedOverRoaming(true)
-        .setTitle("Downloading video")
-        .setDescription("Please wait...")
-        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-        .setDestinationUri(Uri.fromFile(videoFile))
-
-    // Get the DownloadManager system service
-    val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-
-    // Enqueue the download request and get the download ID
-    val downloadId = downloadManager.enqueue(downloadRequest)
-
-    // Create a query to check the download status
-    val downloadQuery = DownloadManager.Query().setFilterById(downloadId)
-
-    // Start a background thread to check the download status
-    Thread {
-        var isDownloaded = false
-        var downloadStatus = DownloadManager.STATUS_FAILED
-        var downloadProgress = 0
-
-        while (!isDownloaded) {
-            // Get the download status from the DownloadManager
-            val cursor = downloadManager.query(downloadQuery)
-            if (cursor.moveToFirst()) {
-                downloadStatus = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
-                downloadProgress =
-                    cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
-                isDownloaded =
-                    downloadStatus == DownloadManager.STATUS_SUCCESSFUL || downloadStatus == DownloadManager.STATUS_FAILED
-            }
-            cursor.close()
-
-            // Update the UI with the download progress
-            runOnUiThread {
-                // Update the UI with the download progress (e.g., using a progress bar)
-            }
-
-            // Wait for a short time before checking the download status again
-            Thread.sleep(500)
-        }
-
-        // Show a toast message when the download is complete
-        if (downloadStatus == DownloadManager.STATUS_SUCCESSFUL) {
-            runOnUiThread {
-                Toast.makeText(this, "Video downloaded successfully", Toast.LENGTH_SHORT).show()
-            }
-
-            // Share the downloaded video file using an intent chooser
-            shareVideoFile(videoFile)
-        } else {
-            runOnUiThread {
-                Toast.makeText(this, "Video download failed", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }.start()
-}
-
-private fun Context.shareVideoFile(videoFile: File) {
-    val sendIntent = Intent().apply {
-        action = Intent.ACTION_SEND
-        type = "video/mp4"
-        putExtra(Intent.EXTRA_TEXT, "title" + "\n\n" + "body")
-        putExtra(Intent.EXTRA_TITLE, "title")
-        putExtra(Intent.EXTRA_SUBJECT, "subject")
-        putExtra(
-            Intent.EXTRA_STREAM,
-            FileProvider.getUriForFile(this@shareVideoFile, "$packageName.fileprovider", videoFile)
-        )
+    // Show a toast indicating the file was saved
+    val toastMessage = "Image saved to cache directory as $imageName"
+    if (cacheFile.exists()) {
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
     }
-    val chooserIntent = Intent.createChooser(sendIntent, "Share Video")
-    chooserIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-    startActivity(chooserIntent)
+}
+
+fun Context.saveImageFromBitmapToDownload(bitmap: Bitmap, imageName: String) {
+    val downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+    val downloadFile = File(downloadDir, imageName)
+    val downloadFileOutputStream = FileOutputStream(downloadFile)
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, downloadFileOutputStream)
+    downloadFileOutputStream.flush()
+    downloadFileOutputStream.close()
+
+    // Notify the system that a new file was added to the downloads directory
+    MediaScannerConnection.scanFile(this, arrayOf(downloadFile.absolutePath), null, null)
+
+    // Show a toast indicating the file was saved
+    val toastMessage = "Image saved to downloads directory as $imageName"
+    if (downloadFile.exists()) {
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+    }
 }
